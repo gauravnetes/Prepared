ce5b2711063b652460e9d9099f7547c76435800a
gauravnetes <gouravchandra935@gmail.com>
2025-03-30 11:21:32 +0530

    Home page --default structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const RootLayout = ( {children}: {children: React.ReactNode} ) => {
   return (
-    <div>
-      {children} 
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex items-center gap-2'>
+          <Image src="/logo.svg" alt='Logo' width={38} height={32}
+          />
+          <h2 className='text-primary-100'>PrePared</h2>
+        </Link>
+      </nav>
+      {children}
     </div>
   )
 }
